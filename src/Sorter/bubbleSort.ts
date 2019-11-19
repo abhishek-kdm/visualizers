8dc7fe83e6bb6df5d45b571fc3a25b9f199a3598
Abhishek <abhishekk19@gmail.com>
2019-11-19 23:47:53 +0530

    Bubble Sort implementation.

---
+++ src/Sorter/bubbleSort.ts
@@ -0,0 +1,21 @@
+import BarArray from '../BarArray';
+
+
+const { colors } = BarArray;
+
+const bubbleSort = async (array: BarArray) => {
+
+  await array.loop(array.length - 1, 1, -1, async (i) => {
+
+    await array.loop(0, i - 1, 1, (j) => {
+      if (array.valueAt(j) > array.valueAt(j + 1)) {
+        array.swap(j, j + 1);
+      }
+    });
+
+  }, { colors: { processing: colors.IDEAL, done: colors.DONE } });
+  await array.completed();
+}
+
+export default bubbleSort;
+
---
+++ src/Sorter/index.ts
@@ -0,0 +1,12 @@
+import bubbleSort from './bubbleSort';
+import insertionSort from './insertionSort';
+import selectionSort from './selectionSort';
+import quickSort from './quickSort';
+
+export default {
+  bubbleSort,
+  insertionSort,
+  selectionSort,
+  quickSort
+};
+
