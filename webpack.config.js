fb655412c28307397e3b27d5a1fe52e791cb249a
Abhishek <abhishekk19@gmail.com>
2019-11-19 23:46:07 +0530

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+
+module.exports = (env) => ({
+  entry: resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: env.production ? '[name].[chunkhash].js' : '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  }
+});
+
