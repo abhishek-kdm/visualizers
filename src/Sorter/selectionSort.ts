aefa3294798e23bce9659c59c4bb8daf0bbb0e84
Abhishek <abhishekk19@gmail.com>
2019-11-19 23:48:28 +0530

    Selection Sort implementation.

---
+++ src/Sorter/selectionSort.ts
@@ -0,0 +1,29 @@
+import BarArray from '../BarArray';
+
+
+const { colors } = BarArray;
+
+const selectionSort = async (array: BarArray) => {
+  const len = array.length;
+
+  await array.loop(0, len - 2, 1, async (i) => {
+    let minIndex = i;
+
+    await array.loop(i, len - 1, 1, (j) => {
+      if (array.valueAt(j) < array.valueAt(minIndex))
+        minIndex = j;
+    });
+
+    array.swap(i, minIndex);
+
+    // edge case (can't help it).
+    array.paint([len - 1], colors.IDEAL)
+
+  }, { colors: { done: colors.DONE } });
+
+  await array.completed();
+}
+
+
+export default selectionSort;
+
