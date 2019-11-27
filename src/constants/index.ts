c5e60cadb2ba05ac79e425761704e6f60fa46f3d
Abhishek <abhishekk19@gmail.com>
2019-11-27 21:03:49 +0530

    constants.

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_START_X: number = 10;
+export const DEFAULT_START_Y: number = 500;
+export const DEFAULT_BAR_WIDTH: number = 10;
+export const DEFAULT_BAR_SPACING: number = 4;
+
+
