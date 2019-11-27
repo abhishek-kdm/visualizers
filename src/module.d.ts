c412c65878ae9364e3aed582edc0662e523031c6
Abhishek <abhishekk19@gmail.com>
2019-11-27 21:04:27 +0530

    types and interfaces seperated.

---
+++ src/module.d.ts
@@ -0,0 +1,16 @@
+type Statement = 'break' | 'continue' | void;
+
+interface BarArrayStats { [stats: string]: string | null }
+
+interface Bar {
+  value: number
+  color: string
+}
+
+interface BarArrayDrawingOptions {
+  start_x: number
+  start_y: number
+  width: number
+  spacing: number
+}
+
