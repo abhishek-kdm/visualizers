f5ef19eacb170f95ba76db40f2df797ec06919a6
Abhishek <abhishekk19@gmail.com>
2019-12-03 13:12:35 +0530

    default stats structure change

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,19 @@
 export const DEFAULT_START_X: number = 10;
 export const DEFAULT_START_Y: number = 500;
 export const DEFAULT_BAR_WIDTH: number = 10;
-export const DEFAULT_BAR_SPACING: number = 4;
+export const DEFAULT_BAR_SPACING: number = 1;
+
+// export const DEFAULT_STATISTICS = {
+//   A: null,
+//   L: null,
+//   N: null,
+// };
+export const DEFAULT_STATISTICS = {
+  Algorithm: null,
+  Length: null,
+  accessed: null,
+};
+
+export const DEFAULT_DELAY = 0; // in ms.
 
 
