3ee990cacc5731e4ca628c6c84ce42d06dcd3f2b
Abhishek <abhishekk19@gmail.com>
2019-12-03 13:12:07 +0530

    removed reshuffle (shifted to controller).

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,20 +1,13 @@
 import BarArray from '../BarArray';
-import { randArray } from '../utils';
 
 
 export const BarArrayProvider = function(this: any) {
 
-  this.generateBarArray = function() {
+  this.generateBarArray = (arr: number[]): BarArray => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    const arr = randArray(34);
 
     const array = new BarArray(canvas, arr);
     return array;
-  }
-
-  this.reshuffle = (array: BarArray) => {
-    array.generateArray(randArray(34));
-    array.toInitialState();
   }
 };
 
