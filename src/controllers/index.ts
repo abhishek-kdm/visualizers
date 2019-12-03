97793a84eeb68c673eda8a586a624963ac71b7bf
Abhishek <abhishekk19@gmail.com>
2019-12-03 13:10:32 +0530

    eventlistener, some helper functions, etc.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,27 +1,53 @@
-import Sorter from '../Sorter';
+// eslint-disable-next-line no-unused-vars
+import Sorter, { SorterFunction } from '../Sorter';
+import angular from 'angular';
+
+import { randArray, jsonify } from '../utils';
+import BarArray from '../BarArray';
 
 
 export const SortVisualizerCtrl = function(
   $scope: any,
   BarArrayProvider: any
 ) {
-  const array = BarArrayProvider.generateBarArray();
 
-  $scope.reshuffle = () => BarArrayProvider.reshuffle(array);
-  $scope.reset = array.toInitialState.bind(array);
+  $scope.states = Object.assign({}, BarArray.states);
+  $scope.state = $scope.states.IDEAL;
 
+  window.addEventListener(BarArray.stateEventName, (e: any) => {
+    $scope.state = e.detail.state;
+    $scope.$evalAsync();
+  });
+
+  $scope.array = [];
+  $scope.arrayUpdater = (value: string) => angular.isDefined(value) ?
+    ($scope.array = jsonify(value)) : $scope.array.map(String).join(', ');
+
+
+  const array = BarArrayProvider.generateBarArray($scope.array);
+
+  $scope.handlers = {
+    reset: array.toInitialState.bind(array),
+
+    randomize: () => {
+      $scope.array = randArray(75);
+      array.generateArray($scope.array);
+      array.toInitialState();
+    },
+
+    sort: (funcName: string) => {
+      array.generateArray($scope.array);
+      (Sorter as { [name: string]: SorterFunction })[funcName](array);
+    },
+  }
+
+  $scope.F = 'bubbleSort';
   $scope.options = [
-    { label: 'Bubble Sort', value: 0 },
-    { label: 'Insertion Sort', value: 1 },
-    { label: 'Selection Sort', value: 2 },
+    { label: 'Bubble Sort', value: 'bubbleSort' },
+    { label: 'Insertion Sort', value: 'insertionSort' },
+    { label: 'Selection Sort', value: 'selectionSort' },
   ];
 
-  $scope.F = 0;
-  $scope.sorters = [
-    (() => { Sorter.bubbleSort(array); }),
-    (() => { Sorter.insertionSort(array); }),
-    (() => { Sorter.selectionSort(array); }),
-  ];
 
 }
 
